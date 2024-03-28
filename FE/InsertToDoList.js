953414f70fd9dc2e1eeca78e5e544041d24b91dc
nguyenchanhdat <chanhdat2504@gmail.com>
2024-03-28 21:07:22 +0700

    Creat folders

---
+++ FE/InsertToDoList.js
@@ -0,0 +1,14 @@
+function insertData(toDo) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "http://localhost:8080");
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  const toDoJSONStringfy = JSON.stringify(toDo);
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 201) {
+      console.log(JSON.parse(xhr.responseText));
+    } else {
+      console.log(`Error: ${xhr.status}`);
+    }
+  };
+  xhr.send(toDoJSONStringfy);
+}
