9627d07a0860da91a57964c6a1e9a0ed2023d79c
nguyenchanhdat <chanhdat2504@gmail.com>
2024-03-29 21:20:55 +0700

    Successfully take post

--- BE/BE.js
+++ BE/BE.js
@@ -1,8 +1,19 @@
 let http = require("http");
+let fs = require("fs");
 
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World!");
+    if (req.method == "POST") {
+      var body = "";
+      req.on("data", function (data) {
+        body += data;
+        console.log("Partial body: " + body);
+      });
+      req.on("end", function () {
+        console.log("Body: " + body);
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end("post received");
+      });
+    }
   })
   .listen(8080);
